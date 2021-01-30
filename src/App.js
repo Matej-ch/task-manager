b7917c9fea8b2b7090f48ddc2b94de344b0095d5
Matej <matejchalachan@gmail.com>
2021-01-30 23:49:06 +0100

    update reminder

--- src/App.js
+++ src/App.js
@@ -21,6 +21,11 @@ function App() {
         return await res.json()
     }
 
+    const fetchTask = async (id) =>  {
+        const res = await fetch(`http://localhost:5000/tasks/${id}`);
+        return await res.json()
+    }
+
     const addTask = async (task) => {
 
         const res =  await fetch('http://localhost:5000/tasks',{
@@ -43,8 +48,20 @@ function App() {
         setTasks(tasks.filter((task) => task.id !== id))
     }
 
-    const toggleReminder = (id) => {
-        setTasks(tasks.map(task => task.id === id ? {...task, reminder: !task.reminder} : task ))
+    const toggleReminder = async (id) => {
+
+        const taskToToggle = await fetchTask(id)
+        const updTask = {...taskToToggle,reminder: !taskToToggle.reminder};
+
+        const res = await fetch(`http://localhost:5000/tasks/${id}`,{method: 'PUT',headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(updTask)
+        })
+
+        const data = await res.json()
+
+        setTasks(tasks.map(task => task.id === id ? {...task, reminder: data.reminder} : task ))
     }
 
     return (
