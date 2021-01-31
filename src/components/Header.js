5ac8f96cd7aa4e0c2b43c0b8c2c8b855f8204fe2
Matej <matejchalachan@gmail.com>
2021-01-31 13:27:05 +0100

    header style added

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,20 @@ import Button from "./Button";
 const Header = ({ title,onAdd,showAdd }) => {
 
     return (
-        <header className="flex justify-between items-center mb-1">
-            <h1>
-                {title}
-            </h1>
-            <Button text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
+        <header className="flex justify-between items-center mb-2 w-full">
+            <div className="mx-auto px-4 sm:px-6 w-full mb-2">
+                <div
+                    className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+                    <div className="flex justify-start lg:w-0 lg:flex-1">
+                        <a href="#" className="font-bold text-xl">
+                            <span>{title}</span>
+                        </a>
+                    </div>
+                    <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+                        <Button text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
+                    </div>
+                </div>
+            </div>
         </header>
     )
 }
